f8b1e4e1378c342ce418db25f89404eac88252b9
byfxxm <byfxxm@outlook.com>
2023-04-02 13:20:30 +0800

    22

--- src/pipeline/pipeline/predicate.h
+++ src/pipeline/pipeline/predicate.h
@@ -117,26 +117,6 @@ namespace byfxxm {
 				}, value);
 		};
 
-		struct _GtagHash {
-			size_t operator()(const Gtag& tag) const {
-				return std::hash<token::Kind>()(tag.code) ^ std::hash<double>()(tag.value);
-			}
-		};
-
-		struct _GtagEqual {
-			bool operator()(const Gtag& tag1, const Gtag& tag2) const {
-				return (tag1.code == tag2.code) && (tag1.value == tag2.value);
-			}
-		};
-
-		inline const std::unordered_map<Gtag, Gfunc, _GtagHash, _GtagEqual> gtag_to_ginterface = {
-			{{token::Kind::G, 0}, &Ginterface::G0},
-			{{token::Kind::G, 1}, &Ginterface::G1},
-			{{token::Kind::G, 2}, &Ginterface::G2},
-			{{token::Kind::G, 3}, &Ginterface::G3},
-			{{token::Kind::G, 4}, &Ginterface::G4},
-		};
-
 		inline auto Gcmd = [](const std::vector<Value>& tags, Ginterface* pimpl, Address& addr)->Value {
 			if (tags.empty())
 				throw AddressException();

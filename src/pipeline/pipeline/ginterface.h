30a2b42439cbb3b8143feeaf86c5ff72a9503902
byfxxm <byfxxm@outlook.com>
2023-04-15 13:10:26 +0800

    22

--- src/pipeline/pipeline/ginterface.h
+++ src/pipeline/pipeline/ginterface.h
@@ -5,8 +5,8 @@
 
 namespace byfxxm {
 	using Gparams = std::vector<Gtag>;
-
 	class Address;
+
 	class Ginterface {
 	public:
 		virtual ~Ginterface() = default;
@@ -15,27 +15,5 @@ namespace byfxxm {
 		virtual bool G2(const Gparams&, const Address&) = 0;
 		virtual bool G3(const Gparams&, const Address&) = 0;
 		virtual bool G4(const Gparams&, const Address&) = 0;
-	};
-
-	using Gfunc = bool(Ginterface::*)(const Gparams&, const Address&);
-
-	struct _GtagHash {
-		size_t operator()(const Gtag& tag) const {
-			return std::hash<token::Kind>()(tag.code) ^ std::hash<double>()(tag.value);
-		}
-	};
-
-	struct _GtagEqual {
-		bool operator()(const Gtag& tag1, const Gtag& tag2) const {
-			return (tag1.code == tag2.code) && (tag1.value == tag2.value);
-		}
-	};
-
-	inline const std::unordered_map<Gtag, Gfunc, _GtagHash, _GtagEqual> gtag_to_ginterface = {
-		{{token::Kind::G, 0}, &Ginterface::G0},
-		{{token::Kind::G, 1}, &Ginterface::G1},
-		{{token::Kind::G, 2}, &Ginterface::G2},
-		{{token::Kind::G, 3}, &Ginterface::G3},
-		{{token::Kind::G, 4}, &Ginterface::G4},
 	};
 }

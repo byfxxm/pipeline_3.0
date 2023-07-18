d224b82acbe43e99e514974c5a320fe8ab8288ee
byfxxm <byfxxm@outlook.com>
2023-07-18 20:45:47 +0800

    22

--- src/pipeline/pipeline/code.h
+++ src/pipeline/pipeline/code.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "array_nd.h"
 
 namespace byfxxm {
+	using AxesArray = ArrayNd<double, 1>;
 	struct Code {};
+
+	struct Move {
+		AxesArray end;
+	};
+
+	struct Line {
+		AxesArray end;
+	};
+
+	struct Arc {
+		AxesArray end;
+		AxesArray center;
+		bool bCcw{ false };
+	};
 }

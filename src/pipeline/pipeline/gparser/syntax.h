8a54e967afeee0f920154c33000199230ca7ed79
byfxxm <byfxxm@outlook.com>
2023-10-05 21:00:28 +0800

    22

--- src/pipeline/pipeline/gparser/syntax.h
+++ src/pipeline/pipeline/gparser/syntax.h
@@ -6,9 +6,12 @@
 #include "block.h"
 #include "memory.h"
 
-namespace byfxxm {
-	inline std::optional<Statement> GetStatement(ClonePtr<block::Block>& block) {
-		if (block) {
+namespace byfxxm
+{
+	inline std::optional<Statement> GetStatement(ClonePtr<block::Block> &block)
+	{
+		if (block)
+		{
 			auto tree = block->Next();
 			if (tree.has_value())
 				return std::move(tree.value());
@@ -22,64 +25,77 @@ namespace byfxxm {
 	using AbstreeWithLineno = std::tuple<Abstree, size_t>;
 
 	template <StreamConcept T>
-	class Syntax {
+	class Syntax
+	{
 	public:
-		Syntax(T&& stream) : _lex(std::move(stream)) {}
+		Syntax(T &&stream) : _lex(std::move(stream)) {}
 
-		std::optional<AbstreeWithLineno> Next() {
+		std::optional<AbstreeWithLineno> Next()
+		{
 			auto stmt = GetStatement(_remain_block);
-			if (stmt) {
+			if (stmt)
+			{
 				assert(std::holds_alternative<Segment>(std::get<0>(stmt.value())));
-				return AbstreeWithLineno{ _ToAbstree(std::get<Segment>(std::move(std::get<0>(stmt.value())))), std::get<1>(stmt.value()) };
+				return AbstreeWithLineno{_ToAbstree(std::get<Segment>(std::move(std::get<0>(stmt.value())))), std::get<1>(stmt.value())};
 			}
 
-			auto get = [this]() {
+			auto get = [this]()
+			{
 				auto tok = _lex.Get();
 				if (tok.kind == token::Kind::NEWLINE)
 					++_lineno;
 				return tok;
-				};
-			auto peek = [this]() {return _lex.Peek(); };
-			auto line = [this]() {return _lineno; };
-			auto get_rval = [this]()->Value {return _return_val; };
-			stmt = GetStatement(grammar::Utils{ get, peek, line, get_rval });
+			};
+			auto peek = [this]()
+			{ return _lex.Peek(); };
+			auto line = [this]()
+			{ return _lineno; };
+			auto get_rval = [this]() -> Value
+			{ return _return_val; };
+			stmt = GetStatement(grammar::Utils{get, peek, line, get_rval});
 			if (!stmt)
 				return {};
 
 			return _ToAbstree(std::move(stmt.value()));
 		}
 
-		void SetEnv(Address* addr, Ginterface* pimpl) {
+		void SetEnv(Address *addr, Ginterface *pimpl)
+		{
 			_addr = addr;
 			_pimpl = pimpl;
 		}
 
 	private:
-		Abstree _ToAbstree(Segment&& seg) {
+		Abstree _ToAbstree(Segment &&seg)
+		{
 			return Abstree(expr(seg), _return_val, _addr, _pimpl);
 		}
 
-		AbstreeWithLineno _ToAbstree(Statement&& stmt) {
+		AbstreeWithLineno _ToAbstree(Statement &&stmt)
+		{
 			return std::visit(
 				Overloaded{
-					[this, line = std::get<1>(stmt)](Segment&& seg)->AbstreeWithLineno {
-						return { _ToAbstree(std::move(seg)), line };
+					[this, line = std::get<1>(stmt)](Segment &&seg) -> AbstreeWithLineno
+					{
+						return {_ToAbstree(std::move(seg)), line};
 					},
-					[this](ClonePtr<block::Block>&& block_)->AbstreeWithLineno {
+					[this](ClonePtr<block::Block> &&block_) -> AbstreeWithLineno
+					{
 						_remain_block = std::move(block_);
 						auto stmt_ = GetStatement(_remain_block);
 						assert(std::holds_alternative<Segment>(std::get<0>(stmt_.value())));
-						return { _ToAbstree(std::get<Segment>(std::move(std::get<0>(stmt_.value())))), std::get<1>(stmt_.value()) };
+						return {_ToAbstree(std::get<Segment>(std::move(std::get<0>(stmt_.value())))), std::get<1>(stmt_.value())};
 					},
-				}, std::move(std::get<0>(stmt)));
+				},
+				std::move(std::get<0>(stmt)));
 		}
 
 	private:
 		Lexer<T> _lex;
-		size_t _lineno{ 1 };
+		size_t _lineno{1};
 		Value _return_val;
-		Address* _addr{ nullptr };
-		Ginterface* _pimpl{ nullptr };
+		Address *_addr{nullptr};
+		Ginterface *_pimpl{nullptr};
 		ClonePtr<block::Block> _remain_block;
 	};
 

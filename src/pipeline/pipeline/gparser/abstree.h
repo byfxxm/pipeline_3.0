376e260a227d4e09b34d16bb1f76cc4146c26823
byfxxm <byfxxm@outlook.com>
2023-10-06 08:45:35 +0800

    22

--- src/pipeline/pipeline/gparser/abstree.h
+++ src/pipeline/pipeline/gparser/abstree.h
@@ -1,100 +1,86 @@
 ﻿#pragma once
-#include <functional>
-#include <variant>
-#include <future>
-#include <cassert>
-#include "token.h"
-#include "predicate.h"
 #include "ginterface.h"
 #include "memory.h"
+#include "predicate.h"
+#include "token.h"
+#include <cassert>
+#include <functional>
+#include <future>
+#include <variant>
 
-namespace byfxxm
-{
-	class Abstree
-	{
-	public:
-		struct Node;
-		using NodePtr = ClonePtr<Node>;
+namespace byfxxm {
+class Abstree {
+public:
+  struct Node;
+  using NodePtr = ClonePtr<Node>;
 
-		struct Node
-		{
-			Predicate pred;
-			std::pmr::vector<NodePtr> subs;
-		};
+  struct Node {
+    Predicate pred;
+    std::pmr::vector<NodePtr> subs;
+  };
 
-		Abstree(NodePtr &&root, Value &rval, Address *addr, Ginterface *pimpl) noexcept
-			: _root(std::move(root)), _return_val(rval), _addr(addr), _pimpl(pimpl)
-		{
-			assert(_root);
-		}
+  Abstree(NodePtr &&root, Value &rval, Address *addr,
+          Ginterface *pimpl) noexcept
+      : _root(std::move(root)), _return_val(rval), _addr(addr), _pimpl(pimpl) {
+    assert(_root);
+  }
 
-		~Abstree() = default;
-		Abstree(const Abstree &) = delete;
-		Abstree(Abstree &&) noexcept = default;
-		Abstree &operator=(const Abstree &) = delete;
-		Abstree &operator=(Abstree &&) noexcept = default;
+  ~Abstree() = default;
+  Abstree(const Abstree &) = delete;
+  Abstree(Abstree &&) noexcept = default;
+  Abstree &operator=(const Abstree &) = delete;
+  Abstree &operator=(Abstree &&) noexcept = default;
 
-		Value Execute()
-		{
-			_return_val = _Execute(_root);
-			return _return_val;
-		}
+  Value Execute() {
+    _return_val = _Execute(_root);
+    return _return_val;
+  }
 
-	private:
-		Value _Execute(const NodePtr &node)
-		{
-			if (std::holds_alternative<Value>(node->pred))
-				return std::get<Value>(node->pred);
+private:
+  Value _Execute(const NodePtr &node) {
+    if (std::holds_alternative<Value>(node->pred))
+      return std::get<Value>(node->pred);
 
-			std::pmr::vector<Value> params;
-			std::ranges::for_each(node->subs, [&](auto &&p)
-								  { params.push_back(_Execute(p)); });
+    std::pmr::vector<Value> params;
+    std::ranges::for_each(node->subs,
+                          [&](auto &&p) { params.push_back(_Execute(p)); });
 
-			return std::visit(
-				Overloaded{
-					[](const Value &value)
-					{
-						return value;
-					},
-					[&](const Unary &unary)
-					{
-						assert(params.size() == 1);
-						return std::visit([&](auto &&func)
-										  { return func(params[0]); },
-										  unary);
-					},
-					[&](const Binary &binary)
-					{
-						assert(params.size() == 2);
-						return std::visit([&](auto &&func)
-										  { return func(params[0], params[1]); },
-										  binary);
-					},
-					[&](const Sharp &sharp)
-					{
-						assert(params.size() == 1);
-						return std::visit([&](auto &&func)
-										  { return func(params[0], _addr); },
-										  sharp);
-					},
-					[&](const Gcmd &gcmd)
-					{
-						assert(!params.empty());
-						return std::visit([&](auto &&func)
-										  { return func(params, _addr, _pimpl); },
-										  gcmd);
-					},
-					[](const auto &) -> Value { // default
-						throw AbstreeException();
-					},
-				},
-				node->pred);
-		}
+    return std::visit(
+        Overloaded{
+            [](const Value &value) { return value; },
+            [&](const Unary &unary) {
+              assert(params.size() == 1);
+              return std::visit([&](auto &&func) { return func(params[0]); },
+                                unary);
+            },
+            [&](const Binary &binary) {
+              assert(params.size() == 2);
+              return std::visit(
+                  [&](auto &&func) { return func(params[0], params[1]); },
+                  binary);
+            },
+            [&](const Sharp &sharp) {
+              assert(params.size() == 1);
+              return std::visit(
+                  [&](auto &&func) { return func(params[0], _addr); }, sharp);
+            },
+            [&](const Gcmd &gcmd) {
+              assert(!params.empty());
+              return std::visit(
+                  [&](auto &&func) { return func(params, _addr, _pimpl); },
+                  gcmd);
+            },
+            [](const auto &) -> Value { // default
+              throw AbstreeException();
+            },
+        },
+        node->pred);
+  }
 
-	private:
-		NodePtr _root;
-		Value &_return_val;
-		Address *_addr{nullptr};
-		Ginterface *_pimpl{nullptr};
-	};
-}
+private:
+  NodePtr _root;
+  Value &_return_val;
+  Address *_addr{nullptr};
+  Ginterface *_pimpl{nullptr};
+};
+} // namespace byfxxm
--- src/pipeline/pipeline/gparser/grammar.h
+++ src/pipeline/pipeline/gparser/grammar.h
@@ -1,317 +1,278 @@
 ﻿#pragma once
-#include "token.h"
 #include "abstree.h"
-#include "production.h"
 #include "block.h"
 #include "memory.h"
+#include "production.h"
+#include "token.h"
 
-namespace byfxxm
-{
-	namespace grammar
-	{
-		using Get = std::function<token::Token()>;
-		using Peek = std::function<token::Token()>;
-		using Line = std::function<size_t()>;
+namespace byfxxm {
+namespace grammar {
+using Get = std::function<token::Token()>;
+using Peek = std::function<token::Token()>;
+using Line = std::function<size_t()>;
 
-		struct Utils
-		{
-			Get get;
-			Peek peek;
-			Line line;
-			GetRetVal return_val;
-		};
+struct Utils {
+  Get get;
+  Peek peek;
+  Line line;
+  GetRetVal return_val;
+};
 
-		inline bool NewSegment(const token::Token &tok)
-		{
-			return tok.kind == token::Kind::NEWLINE || tok.kind == token::Kind::SEMI;
-		}
-
-		inline bool EndOfFile(const token::Token &tok)
-		{
-			return tok.kind == token::Kind::KEOF;
-		}
-
-		inline void SkipNewlines(const Utils &utils)
-		{
-			for (;;)
-			{
-				auto tok = utils.peek();
-				if (tok.kind != token::Kind::NEWLINE)
-					break;
-
-				utils.get();
-			}
-		}
-
-		inline std::optional<Statement> GetStatement(const Utils &);
-
-		class Grammar
-		{
-		public:
-			virtual ~Grammar() = default;
-			virtual bool First(const token::Token &) const = 0;
-			virtual std::optional<Statement> Rest(Segment &&, const Utils &) const = 0;
-		};
-
-		class Blank : public Grammar
-		{
-			virtual bool First(const token::Token &tok) const override
-			{
-				return tok.kind == token::Kind::NEWLINE || tok.kind == token::Kind::SEMI;
-			}
-
-			virtual std::optional<Statement> Rest(Segment &&, const Utils &) const override
-			{
-				return {};
-			}
-		};
-
-		class Expr : public Grammar
-		{
-			virtual bool First(const token::Token &tok) const override
-			{
-				return tok.kind == token::Kind::SHARP || tok.kind == token::Kind::LB;
-			}
-
-			virtual std::optional<Statement> Rest(Segment &&seg, const Utils &utils) const override
-			{
-				for (;;)
-				{
-					auto tok = utils.get();
-					if (NewSegment(tok))
-						break;
-
-					seg.push_back(std::move(tok));
-				}
-
-				return Statement(std::move(seg), utils.line() - 1);
-			}
-		};
-
-		class Ggram : public Grammar
-		{
-			virtual bool First(const token::Token &tok) const override
-			{
-				return IsGcode(tok);
-			}
-
-			virtual std::optional<Statement> Rest(Segment &&seg, const Utils &utils) const override
-			{
-				Segment gtag;
-				for (;;)
-				{
-					auto tok = utils.peek();
-					if (NewSegment(tok))
-					{
-						if (!gtag.empty())
-							seg.push_back(expr(gtag));
-						break;
-					}
-
-					if (!IsGcode(tok))
-					{
-						gtag.push_back(std::move(tok));
-						utils.get();
-						continue;
-					}
-
-					if (gtag.empty())
-					{
-						seg.push_back(std::move(tok));
-						utils.get();
-					}
-					else
-					{
-						seg.push_back(expr(gtag));
-						gtag.clear();
-					}
-				}
-
-				Segment ret;
-				ret.push_back(gtree(seg));
-				return Statement(std::move(ret), utils.line());
-			}
-		};
-
-		class IfElse : public Grammar
-		{
-			virtual bool First(const token::Token &tok) const override
-			{
-				return tok.kind == token::Kind::IF;
-			}
-
-			virtual std::optional<Statement> Rest(Segment &&seg, const Utils &utils) const override
-			{
-				using If = block::IfElse::If;
-				using Else = block::IfElse::Else;
-
-				auto read_cond = [&]() -> Statement
-				{
-					Segment seg;
-					for (;;)
-					{
-						auto tok = utils.get();
-						if (tok.kind == token::Kind::NEWLINE)
-							throw SyntaxException();
-
-						if (tok.kind == token::Kind::THEN)
-							break;
-
-						seg.push_back(std::move(tok));
-					}
-
-					return {std::move(seg), utils.line()};
-				};
-
-				auto read_scope = [&](Scope &scope)
-				{
-					for (;;)
-					{
-						SkipNewlines(utils);
-						auto tok = utils.peek();
-						if (tok.kind == token::Kind::ELSE || tok.kind == token::Kind::ELSEIF || tok.kind == token::Kind::ENDIF)
-							break;
-
-						auto stmt = GetStatement(utils);
-						if (!stmt)
-							break;
-
-						scope.push_back(std::move(stmt.value()));
-					}
-				};
-
-				// read if
-				block::IfElse ifelse(utils.return_val);
-				ifelse._ifs.push_back(If(read_cond()));
-				read_scope(ifelse._ifs.back().scope);
-
-				// read elseif
-				for (;;)
-				{
-					SkipNewlines(utils);
-					auto tok = utils.peek();
-					if (tok.kind != token::Kind::ELSEIF)
-						break;
-
-					ifelse._ifs.push_back(If(read_cond()));
-					read_scope(ifelse._ifs.back().scope);
-				}
-
-				// read else
-				auto tok = utils.peek();
-				if (tok.kind == token::Kind::ELSE)
-				{
-					utils.get();
-					SkipNewlines(utils);
-					read_scope(ifelse._else.scope);
-				}
-
-				// endif
-				tok = utils.get();
-				if (tok.kind != token::Kind::ENDIF)
-					throw SyntaxException();
-
-				return Statement(ClonePtr<block::Block>(MakeUnique<block::IfElse>(std::move(ifelse))), utils.line());
-			}
-		};
-
-		class While : public Grammar
-		{
-			virtual bool First(const token::Token &tok) const override
-			{
-				return tok.kind == token::Kind::WHILE;
-			}
-
-			virtual std::optional<Statement> Rest(Segment &&seg, const Utils &utils) const override
-			{
-				auto read_cond = [&]() -> Statement
-				{
-					Segment seg;
-					for (;;)
-					{
-						auto tok = utils.get();
-						if (tok.kind == token::Kind::NEWLINE)
-							throw SyntaxException();
-
-						if (tok.kind == token::Kind::DO)
-							break;
-
-						seg.push_back(std::move(tok));
-					}
-
-					return {std::move(seg), utils.line()};
-				};
-
-				auto read_scope = [&](Scope &scope)
-				{
-					for (;;)
-					{
-						SkipNewlines(utils);
-						auto tok = utils.peek();
-						if (tok.kind == token::Kind::END)
-							break;
-
-						auto stmt = GetStatement(utils);
-						if (!stmt)
-							break;
-
-						scope.push_back(std::move(stmt.value()));
-					}
-				};
-
-				block::While wh(utils.return_val);
-				wh._cond = read_cond();
-				read_scope(wh._scope);
-
-				// end
-				auto tok = utils.get();
-				if (tok.kind != token::Kind::END)
-					throw SyntaxException();
-
-				return Statement(ClonePtr<block::Block>(MakeUnique<block::While>(std::move(wh))), utils.line());
-			}
-		};
-
-		template <class... _Grams>
-		struct _GrammarsList
-		{
-			static inline const std::unique_ptr<grammar::Grammar> grammars[sizeof...(_Grams)]{
-				std::make_unique<_Grams>()...};
-		};
-
-		using GrammarsList = _GrammarsList<
-			grammar::Blank,
-			grammar::Expr,
-			grammar::Ggram,
-			grammar::IfElse,
-			grammar::While>;
-
-		inline std::optional<Statement> GetStatement(const Utils &utils)
-		{
-			for (;;)
-			{
-				auto tok = utils.peek();
-				if (EndOfFile(tok))
-					return {};
-
-				Segment seg;
-				seg.push_back(utils.get());
-
-				auto iter = std::begin(GrammarsList::grammars);
-				for (; iter != std::end(GrammarsList::grammars); ++iter)
-				{
-					if ((*iter)->First(tok))
-					{
-						std::optional<Statement> sub;
-						if (!(sub = (*iter)->Rest(std::move(seg), utils)).has_value())
-							break;
-						return std::move(sub.value());
-					}
-				}
-
-				if (iter == std::end(GrammarsList::grammars))
-					throw SyntaxException();
-			}
-		}
-	}
+inline bool NewSegment(const token::Token &tok) {
+  return tok.kind == token::Kind::NEWLINE || tok.kind == token::Kind::SEMI;
 }
+
+inline bool EndOfFile(const token::Token &tok) {
+  return tok.kind == token::Kind::KEOF;
+}
+
+inline void SkipNewlines(const Utils &utils) {
+  for (;;) {
+    auto tok = utils.peek();
+    if (tok.kind != token::Kind::NEWLINE)
+      break;
+
+    utils.get();
+  }
+}
+
+inline std::optional<Statement> GetStatement(const Utils &);
+
+class Grammar {
+public:
+  virtual ~Grammar() = default;
+  virtual bool First(const token::Token &) const = 0;
+  virtual std::optional<Statement> Rest(Segment &&, const Utils &) const = 0;
+};
+
+class Blank : public Grammar {
+  virtual bool First(const token::Token &tok) const override {
+    return tok.kind == token::Kind::NEWLINE || tok.kind == token::Kind::SEMI;
+  }
+
+  virtual std::optional<Statement> Rest(Segment &&,
+                                        const Utils &) const override {
+    return {};
+  }
+};
+
+class Expr : public Grammar {
+  virtual bool First(const token::Token &tok) const override {
+    return tok.kind == token::Kind::SHARP || tok.kind == token::Kind::LB;
+  }
+
+  virtual std::optional<Statement> Rest(Segment &&seg,
+                                        const Utils &utils) const override {
+    for (;;) {
+      auto tok = utils.get();
+      if (NewSegment(tok))
+        break;
+
+      seg.push_back(std::move(tok));
+    }
+
+    return Statement(std::move(seg), utils.line() - 1);
+  }
+};
+
+class Ggram : public Grammar {
+  virtual bool First(const token::Token &tok) const override {
+    return IsGcode(tok);
+  }
+
+  virtual std::optional<Statement> Rest(Segment &&seg,
+                                        const Utils &utils) const override {
+    Segment gtag;
+    for (;;) {
+      auto tok = utils.peek();
+      if (NewSegment(tok)) {
+        if (!gtag.empty())
+          seg.push_back(expr(gtag));
+        break;
+      }
+
+      if (!IsGcode(tok)) {
+        gtag.push_back(std::move(tok));
+        utils.get();
+        continue;
+      }
+
+      if (gtag.empty()) {
+        seg.push_back(std::move(tok));
+        utils.get();
+      } else {
+        seg.push_back(expr(gtag));
+        gtag.clear();
+      }
+    }
+
+    Segment ret;
+    ret.push_back(gtree(seg));
+    return Statement(std::move(ret), utils.line());
+  }
+};
+
+class IfElse : public Grammar {
+  virtual bool First(const token::Token &tok) const override {
+    return tok.kind == token::Kind::IF;
+  }
+
+  virtual std::optional<Statement> Rest(Segment &&seg,
+                                        const Utils &utils) const override {
+    using If = block::IfElse::If;
+    using Else = block::IfElse::Else;
+
+    auto read_cond = [&]() -> Statement {
+      Segment seg;
+      for (;;) {
+        auto tok = utils.get();
+        if (tok.kind == token::Kind::NEWLINE)
+          throw SyntaxException();
+
+        if (tok.kind == token::Kind::THEN)
+          break;
+
+        seg.push_back(std::move(tok));
+      }
+
+      return {std::move(seg), utils.line()};
+    };
+
+    auto read_scope = [&](Scope &scope) {
+      for (;;) {
+        SkipNewlines(utils);
+        auto tok = utils.peek();
+        if (tok.kind == token::Kind::ELSE || tok.kind == token::Kind::ELSEIF ||
+            tok.kind == token::Kind::ENDIF)
+          break;
+
+        auto stmt = GetStatement(utils);
+        if (!stmt)
+          break;
+
+        scope.push_back(std::move(stmt.value()));
+      }
+    };
+
+    // read if
+    block::IfElse ifelse(utils.return_val);
+    ifelse._ifs.push_back(If(read_cond()));
+    read_scope(ifelse._ifs.back().scope);
+
+    // read elseif
+    for (;;) {
+      SkipNewlines(utils);
+      auto tok = utils.peek();
+      if (tok.kind != token::Kind::ELSEIF)
+        break;
+
+      ifelse._ifs.push_back(If(read_cond()));
+      read_scope(ifelse._ifs.back().scope);
+    }
+
+    // read else
+    auto tok = utils.peek();
+    if (tok.kind == token::Kind::ELSE) {
+      utils.get();
+      SkipNewlines(utils);
+      read_scope(ifelse._else.scope);
+    }
+
+    // endif
+    tok = utils.get();
+    if (tok.kind != token::Kind::ENDIF)
+      throw SyntaxException();
+
+    return Statement(
+        ClonePtr<block::Block>(MakeUnique<block::IfElse>(std::move(ifelse))),
+        utils.line());
+  }
+};
+
+class While : public Grammar {
+  virtual bool First(const token::Token &tok) const override {
+    return tok.kind == token::Kind::WHILE;
+  }
+
+  virtual std::optional<Statement> Rest(Segment &&seg,
+                                        const Utils &utils) const override {
+    auto read_cond = [&]() -> Statement {
+      Segment seg;
+      for (;;) {
+        auto tok = utils.get();
+        if (tok.kind == token::Kind::NEWLINE)
+          throw SyntaxException();
+
+        if (tok.kind == token::Kind::DO)
+          break;
+
+        seg.push_back(std::move(tok));
+      }
+
+      return {std::move(seg), utils.line()};
+    };
+
+    auto read_scope = [&](Scope &scope) {
+      for (;;) {
+        SkipNewlines(utils);
+        auto tok = utils.peek();
+        if (tok.kind == token::Kind::END)
+          break;
+
+        auto stmt = GetStatement(utils);
+        if (!stmt)
+          break;
+
+        scope.push_back(std::move(stmt.value()));
+      }
+    };
+
+    block::While wh(utils.return_val);
+    wh._cond = read_cond();
+    read_scope(wh._scope);
+
+    // end
+    auto tok = utils.get();
+    if (tok.kind != token::Kind::END)
+      throw SyntaxException();
+
+    return Statement(
+        ClonePtr<block::Block>(MakeUnique<block::While>(std::move(wh))),
+        utils.line());
+  }
+};
+
+template <class... _Grams> struct _GrammarsList {
+  static inline const std::unique_ptr<grammar::Grammar>
+      grammars[sizeof...(_Grams)]{std::make_unique<_Grams>()...};
+};
+
+using GrammarsList =
+    _GrammarsList<grammar::Blank, grammar::Expr, grammar::Ggram,
+                  grammar::IfElse, grammar::While>;
+
+inline std::optional<Statement> GetStatement(const Utils &utils) {
+  for (;;) {
+    auto tok = utils.peek();
+    if (EndOfFile(tok))
+      return {};
+
+    Segment seg;
+    seg.push_back(utils.get());
+
+    auto iter = std::begin(GrammarsList::grammars);
+    for (; iter != std::end(GrammarsList::grammars); ++iter) {
+      if ((*iter)->First(tok)) {
+        std::optional<Statement> sub;
+        if (!(sub = (*iter)->Rest(std::move(seg), utils)).has_value())
+          break;
+        return std::move(sub.value());
+      }
+    }
+
+    if (iter == std::end(GrammarsList::grammars))
+      throw SyntaxException();
+  }
+}
+} // namespace grammar
+} // namespace byfxxm

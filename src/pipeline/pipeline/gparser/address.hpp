11e410914559f83947e0ceeb954193d3249a7fc7
byfxxm <byfxxm@outlook.com>
2023-10-29 14:02:06 +0800

    22

--- src/pipeline/pipeline/gparser/address.hpp
+++ src/pipeline/pipeline/gparser/address.hpp
@@ -23,18 +23,12 @@ public:
     }
   }
 
-  _Value &operator[](const _Key &key) {
+  [[nodiscard]] _Value &operator[](const _Key &key) {
     if (_dict.find(key) == _dict.end())
       _dict.insert(std::make_pair(key, MakeUnique<_VType>(mempool, nan)));
 
     return _dict.at(key);
   }
-
-  const _Value &operator[](const _Key &key) const { return _dict.at(key); }
-
-  bool Contains(const _Key &key) const { return _dict.contains(key); }
-
-  void Clear() { _dict.clear(); }
 
 private:
   std::pmr::unordered_map<_Key, _Value> _dict{&mempool};

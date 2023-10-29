3584d290fc7d3facd655ca42a9daecc4cff9fea0
byfxxm <byfxxm@outlook.com>
2023-10-29 14:09:41 +0800

    22

--- src/pipeline/pipeline/gparser/address.hpp
+++ src/pipeline/pipeline/gparser/address.hpp
@@ -14,15 +14,6 @@ public:
   using _VType = double;
   using _Value = UniquePtr<_VType>;
 
-  Address() = default;
-
-  Address(std::initializer_list<std::pair<_Key, _VType>> list) {
-    for (auto &x : list) {
-      _dict.insert(
-          std::make_pair(x.first, MakeUnique<_VType>(mempool, x.second)));
-    }
-  }
-
   [[nodiscard]] _Value &operator[](const _Key &key) {
     if (_dict.find(key) == _dict.end())
       _dict.insert(std::make_pair(key, MakeUnique<_VType>(mempool, nan)));

25bdb5c39f0b2ad4e00e35851f78f1db5e7be391
byfxxm <byfxxm@outlook.com>
2023-12-07 07:44:20 +0800

    22

--- src/pipeline/pipeline/gparser/address.hpp
+++ src/pipeline/pipeline/gparser/address.hpp
@@ -7,30 +7,33 @@
 namespace byfxxm {
 class Address {
 public:
+  using _Key = double;
+  using _Value = SharpValue;
+
   Address() = default;
 
-  Address(std::initializer_list<std::pair<double, SharpValue>> list) {
+  Address(std::initializer_list<std::pair<_Key, _Value>> list) {
     for (auto &elem : list) {
       Insert(elem.first, elem.second);
     }
   }
 
-  [[nodiscard]] SharpValue &operator[](double key) {
+  [[nodiscard]] _Value &operator[](const _Key &key) {
     if (_dict.find(key) == _dict.end()) {
       auto point = std::make_unique<double>(nan);
-      Insert(key, SharpValue(point.get()));
+      Insert(key, _Value(point.get()));
       _buffer.push_back(std::move(point));
     }
 
     return _dict.at(key);
   }
 
-  void Insert(double key, const SharpValue &sharp) {
+  void Insert(const _Key &key, const _Value &sharp) {
     _dict.insert(std::make_pair(key, sharp));
   }
 
 private:
-  std::unordered_map<double, SharpValue> _dict;
+  std::unordered_map<_Key, SharpValue> _dict;
   std::vector<std::unique_ptr<double>> _buffer;
 };
 } // namespace byfxxm
